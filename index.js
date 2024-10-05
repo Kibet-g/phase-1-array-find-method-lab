56cc3f2b2f6e886040880a9a41f6b08ac7197c7e
+ <kibetg984@gmail.com>
2024-10-05 08:39:25 +0300

    Used find() to look for the object where the result is W ANd if a win return the year otherwise return undefined

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(record) {
+    // Use find() to look for the object where result is "W"
+    let winRecord = record.find(game => game.result === "W");
+  
+    // If a win is found, return the year, otherwise return undefined
+    return winRecord ? winRecord.year : undefined;
+  }
+  
